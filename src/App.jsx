12954ae2206a0cd0646c34aeaddd2d187ff8767d
Will-Marco <mr.marco0101@gmail.com>
2023-12-26 21:17:50 +0500

    add getting data  function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { getData } from "./constants/db.js";
+import "./App.css"
+
+const courses = getData();
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <h1>All Courses</h1>
+      {/* Cart */}
+      <div className="cards_container">
+        {courses.map((course) => (
+          <>
+            <h3>{course.title}</h3>
+          </>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
