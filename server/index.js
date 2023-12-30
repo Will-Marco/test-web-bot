a803527d6907815f8a4cf0875b1d2b5c969eaab7
Will-Marco <mr.marco0101@gmail.com>
2023-12-30 10:54:05 +0500

    add inline keybord functin and server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import express from "express";
+import cors from "cors";
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
@@ -6,9 +8,14 @@ dotenv.config();
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.setMyCommands([
-  { command: "/start", description: "Kurslar haqida ma'lumot" },
+  { command: "/start", description: "Get started" },
+  { command: "/courses", description: "All courses" },
 ]);
 
 bot.on("message", async (msg) => {
@@ -25,6 +32,27 @@ bot.on("message", async (msg) => {
             [
               {
                 text: "Kurslarni ko'rish",
+                web_app: {
+                  url: "https://test-telegram-web-bot.vercel.app",
+                },
+              },
+            ],
+          ],
+        },
+      }
+    );
+  }
+
+  if (text === "/courses") {
+    await bot.sendMessage(
+      chatId,
+      "Assalamu Alaykum\nYou can purchase courses on this platform",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "All courses",
                 web_app: {
                   url: "https://test-telegram-web-bot.vercel.app",
                 },
@@ -63,4 +91,33 @@ bot.on("message", async (msg) => {
       console.log(error);
     }
   }
+});
+
+app.post("/web-data", async (req, res) => {
+  const { products, queryID } = req.body;
+
+  try {
+    await bot.answerWebAppQuery(queryID, {
+      type: "article",
+      id: queryID,
+      title: "You have successfully purchased",
+      input_message_content: {  
+        message_text: `Congratulations on your purchase, You purchased product worth ${products
+          .reduce((a, b) => a + b.price * b.quantity, 0)
+          .toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}\n${products.map((c) => `${c.title} ${c.quantity}X`).join(", ")}`,
+      },
+    });
+    return res.status(200).json({});
+  } catch (error) {
+    return res.status(500).json({});
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}`);
 });
